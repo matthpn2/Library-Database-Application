7e0ec5cb66f9aa19f883599894b762ca49aa3ce8
Matthew Nguyen <matthpn2@uci.edu>
2018-12-13 15:56:04 -0800

    changed spacing

--- backend_database.py
+++ backend_database.py
@@ -6,47 +6,47 @@ class Database:
     '''
 
     def __init__(self, db):
-        self._connection = sqlite3.connect(db)
-        self._cursor = self._connection.cursor()
-        self._cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
-        self._connection.commit()
+        self.connection = sqlite3.connect(db)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+        self.connection.commit()
 
     def viewData(self):
         '''
             Fetches and returns all the rows in the books table.
         '''
-        self._cursor.execute("SELECT * FROM books")
-        rows = self._cursor.fetchall()
+        self.cursor.execute("SELECT * FROM books")
+        rows = self.cursor.fetchall()
         return rows
     
     def insertData(self, title, author, year, isbn):
         '''
             Inserts data into the books table, given user input.
         ''' 
-        self._cursor.execute("INSERT INTO books VALUES (NULL, ?, ?, ? , ?)", (title, author, year, isbn))
-        self._connection.commit()
+        self.cursor.execute("INSERT INTO books VALUES (NULL, ?, ?, ? , ?)", (title, author, year, isbn))
+        self.connection.commit()
 
     def searchData(self, title = "", author = "", year = "", isbn = ""):
         '''
             Fetchs and returns the rows of the books table that matches the user search query.
         '''
-        self._cursor.execute("SELECT * FROM books WHERE title = ? OR author = ? or year = ? or isbn = ?", (title, author, year, isbn))
-        rows = self._cursor.fetchall()
+        self.cursor.execute("SELECT * FROM books WHERE title = ? OR author = ? or year = ? or isbn = ?", (title, author, year, isbn))
+        rows = self.cursor.fetchall()
         return rows
 
     def deleteData(self, id):
         '''
             Deletes data from the books table, using the book id.
         '''
-        self._cursor.execute("DELETE FROM books WHERE id = ?", (id,))
-        self._connection.commit()
+        self.cursor.execute("DELETE FROM books WHERE id = ?", (id,))
+        self.connection.commit()
 
     def updateData(self, id, title, author, year, isbn):
         '''
             Updates data from books table, using the book id.
         '''
-        self._cursor.execute("UPDATE books SET title = ?, author = ?, year = ?, isbn = ? WHERE id = ?", (title, author, year, isbn, id))
-        self._connection.commit()
+        self.cursor.execute("UPDATE books SET title = ?, author = ?, year = ?, isbn = ? WHERE id = ?", (title, author, year, isbn, id))
+        self.connection.commit()
 
     def __del__(self):
-        self._connection.close()
+        self.connection.close()
--- frontend_window.py
+++ frontend_window.py
@@ -8,92 +8,92 @@ class Window(object):
     '''
 
     def __init__(self, window, db):
-        self._window = window
-        self._window.wm_title("Library")
+        self.window = window
+        self.window.wm_title("Library")
 
-        self._db = database
+        self.db = database
 
         # generate labels for Title, Author, Year and ISBN
-        labelTitle = Label(self._window, text = "Title")
-        labelTitle.grid(row = 0, column = 0)
+        labelTitle = Label(self.window, text="Title")
+        labelTitle.grid(row=0, column=0)
 
-        labelAuthor = Label(self._window, text = "Author")
-        labelAuthor.grid(row = 0, column = 2)
+        labelAuthor = Label(self.window, text="Author")
+        labelAuthor.grid(row=0, column=2)
 
-        labelYear = Label(self._window, text = "Year")
-        labelYear.grid(row = 1, column = 0)
+        labelYear = Label(self.window, text="Year")
+        labelYear.grid(row=1, column=0)
 
-        labelISBN = Label(self._window, text = "ISBN")
-        labelISBN.grid(row = 1, column = 2)
+        labelISBN = Label(self.window, text="ISBN")
+        labelISBN.grid(row=1, column=2)
 
         # generate corresponding entry fields for the labels
-        self._titleValue = StringVar()
-        self._entryTitle = Entry(self._window, textvariable = self._titleValue)
-        self._entryTitle.grid(row = 0, column = 1)
+        self.titleValue = StringVar()
+        self.entryTitle = Entry(self.window, textvariable=self.titleValue)
+        self.entryTitle.grid(row=0, column=1)
 
-        self._authorValue = StringVar()
-        self._entryAuthor = Entry(self._window, textvariable = self._authorValue)
-        self._entryAuthor.grid(row = 0, column = 3)
+        self.authorValue = StringVar()
+        self.entryAuthor = Entry(self.window, textvariable=self.authorValue)
+        self.entryAuthor.grid(row=0, column=3)
 
-        self._yearValue = StringVar()
-        self._entryYear = Entry(self._window, textvariable = self._yearValue)
-        self._entryYear.grid(row = 1, column = 1)
+        self.yearValue = StringVar()
+        self.entryYear = Entry(self.window, textvariable=self.yearValue)
+        self.entryYear.grid(row=1, column=1)
 
-        self._isbnValue = StringVar()
-        self._entryISBN = Entry(self._window, textvariable = self._isbnValue)
-        self._entryISBN.grid(row = 1, column = 3)
+        self.isbnValue = StringVar()
+        self.entryISBN = Entry(self.window, textvariable=self.isbnValue)
+        self.entryISBN.grid(row=1, column=3)
 
         # generate list box for displaying information
-        self._listDesc = Listbox(self._window, height = 6, width = 35)
-        self._listDesc.grid(row = 2, column = 0, rowspan = 6, columnspan = 2)
-        self._listDesc.bind("<<ListboxSelect>>", self.getSelectedRow)
+        self.listDesc = Listbox(self.window, height=6, width=35)
+        self.listDesc.grid(row=2, column=0, rowspan=6, columnspan=2)
+        self.listDesc.bind("<<ListboxSelect>>", self.getSelectedRow)
 
         # create scrollbar for list box window
-        scroll = Scrollbar(self._window)
-        scroll.grid(row = 2, column = 2, rowspan = 6)
+        scroll = Scrollbar(self.window)
+        scroll.grid(row=2, column=2, rowspan=6)
 
         # configure list box to add scroll bar object
-        self._listDesc.configure(yscrollcommand = scroll.set)
-        scroll.configure(command = self._listDesc.yview)
+        self.listDesc.configure(yscrollcommand=scroll.set)
+        scroll.configure(command=self.listDesc.yview)
 
         # generate buttons for viewing, clearing searching, adding, updating and deleting
-        buttonView = Button(self._window, text = "View All", width = 12, command = self.viewCommand)
-        buttonView.grid(row = 2, column = 3)
+        buttonView = Button(self.window, text="View All", width=12, command=self.viewCommand)
+        buttonView.grid(row=2, column=3)
 
-        buttonClose = Button(self._window, text = "Clear Entry", width = 12, command = self.clearCommand)
-        buttonClose.grid(row = 3, column = 3)
+        buttonClose = Button(self.window, text= "Clear Entry", width=12, command=self.clearCommand)
+        buttonClose.grid(row=3, column=3)
 
-        buttonSearch = Button(self._window, text = "Search Entry", width = 12, command = self.searchCommand)
-        buttonSearch.grid(row = 4, column = 3)
+        buttonSearch = Button(self.window, text="Search Entry", width=12, command=self.searchCommand)
+        buttonSearch.grid(row=4, column=3)
 
-        buttonAdd = Button(self._window, text = "Add Entry", width = 12, command = self.insertCommand)
-        buttonAdd.grid(row = 5, column = 3)
+        buttonAdd = Button(self.window, text="Add Entry", width=12, command=self.insertCommand)
+        buttonAdd.grid(row=5, column=3)
 
-        buttonUpdate = Button(self._window, text = "Update", width = 12, command = self.updateCommand)
-        buttonUpdate.grid(row = 6, column = 3)
+        buttonUpdate = Button(self.window, text="Update", width=12, command=self.updateCommand)
+        buttonUpdate.grid(row=6, column=3)
 
-        buttonDelete = Button(self._window, text = "Delete", width = 12, command = self.deleteCommand)
-        buttonDelete.grid(row = 7, column = 3)
+        buttonDelete = Button(self.window, text="Delete", width=12, command=self.deleteCommand)
+        buttonDelete.grid(row=7, column=3)
 
     def getSelectedRow(self, event):
         '''
             Fill entries with selected tuple in the list box.
         '''
         try:
-            index = self._listDesc.curselection()[0]
-            self._selectedTuple = self._listDesc.get(index)
+            index = self.listDesc.curselection()[0]
+            self.selectedTuple = self.listDesc.get(index)
 
-            self._entryTitle.delete(0, END)
-            self._entryTitle.insert(END, self._selectedTuple[1])
+            self.entryTitle.delete(0, END)
+            self.entryTitle.insert(END, self.selectedTuple[1])
 
-            self._entryAuthor.delete(0, END)
-            self._entryAuthor.insert(END, self._selectedTuple[2])
+            self.entryAuthor.delete(0, END)
+            self.entryAuthor.insert(END, self.selectedTuple[2])
 
-            self._entryYear.delete(0, END)
-            self._entryYear.insert(END, self._selectedTuple[3])
+            self.entryYear.delete(0, END)
+            self.entryYear.insert(END, self.selectedTuple[3])
 
-            self._entryISBN.delete(0, END)
-            self._entryISBN.insert(END, self._selectedTuple[4])
+            self.entryISBN.delete(0, END)
+            self.entryISBN.insert(END, self.selectedTuple[4])
         except IndexError:
             pass
     
@@ -101,52 +101,50 @@ class Window(object):
         '''
             Clear out all entry fields.
         '''
-        self._entryTitle.delete(0, END)
-        self._entryAuthor.delete(0, END)
-        self._entryYear.delete(0, END)
-        self._entryISBN.delete(0, END)
+        self.entryTitle.delete(0, END)
+        self.entryAuthor.delete(0, END)
+        self.entryYear.delete(0, END)
+        self.entryISBN.delete(0, END)
 
     def viewCommand(self):
         '''
             Get data from backend SQL view query and insert into the list box.
         '''
         # clear list box before displaying data
-        self._listDesc.delete(0, END)
-
-        for row in self._db.viewData():
-            self._listDesc.insert(END, row)
+        self.listDesc.delete(0, END)
+        for row in self.db.viewData():
+            self.listDesc.insert(END, row)
 
     def searchCommand(self):
         ''' 
             Get values from entry widgets and insert data from backend SQL search query into the list box.
         '''
         # clear list box before searching data
-        self._listDesc.delete(0, END)
-
-        for row in self._db.searchData(self._titleValue.get(), self._authorValue.get(), self._yearValue.get(), self._isbnValue.get()):
-            self._listDesc.insert(END, row)
+        self.listDesc.delete(0, END)
+        for row in self.db.searchData(self.titleValue.get(), self.authorValue.get(), self.yearValue.get(), self.isbnValue.get()):
+            self.listDesc.insert(END, row)
 
     def insertCommand(self):
         '''
             Inserts data into the books table, using the backend SQL insert query.
         '''
-        self._db.insertData(self._titleValue.get(), self._authorValue.get(), self._yearValue.get(), self._isbnValue.get())
+        self.db.insertData(self.titleValue.get(), self.authorValue.get(), self.yearValue.get(), self.isbnValue.get())
         
         # clear list box before displaying data
-        self._listDesc.delete(0, END)
-        self._listDesc.insert(END, (self._titleValue.get(), self._authorValue.get(), self._yearValue.get(), self._isbnValue.get()))
+        self.listDesc.delete(0, END)
+        self.listDesc.insert(END, (self.titleValue.get(), self.authorValue.get(), self.yearValue.get(), self.isbnValue.get()))
 
     def updateCommand(self):
         '''
             Updates data in the books table, using the backend SQL update query.
         '''
-        self._db.updateData(self._selectedTuple[0], self._titleValue.get(), self._authorValue.get(), self._yearValue.get(), self._isbnValue.get())
+        self.db.updateData(self.selectedTuple[0], self.titleValue.get(), self.authorValue.get(), self.yearValue.get(), self.isbnValue.get())
     
     def deleteCommand(self):
         '''
             Deletes data in books table, given an ID and using the backend SQL delete query.
         '''
-        self._db.deleteData(self._selectedTuple[0])
+        self.db.deleteData(self.selectedTuple[0])
 
 if __name__ == "__main__":
     # create empty window object instance
@@ -156,5 +154,4 @@ if __name__ == "__main__":
     database = Database("books.db")
 
     Window(window, database)
-
     window.mainloop()
